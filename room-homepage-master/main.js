949606a8aecb5536053981fe74f10f19ea897c27
johnson <kryzbone@gmail.com>
2020-09-25 21:48:40 +0000

    finished desktop layout

---
+++ room-homepage-master/main.js
@@ -0,0 +1,16 @@
+//Selectors
+const leftButton = document.querySelector(".left");
+const rightButton = document.querySelector(".right")
+
+
+//Events
+rightButton.addEventListener("click", () => {
+	console.log("right")
+})
+
+leftButton.addEventListener("click", () => {
+	console.log("left")
+})
+
+
+
