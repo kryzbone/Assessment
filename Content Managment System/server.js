0653d6fb5ea4c01b6843b4a7bdf00b3b2368f728
johnson <kryzbone@gmail.com>
2020-09-26 19:13:23 +0000

    created handler function

--- Content Managment System/server.js
+++ Content Managment System/server.js
@@ -3,9 +3,10 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 //create server
-const server = http.createServer((req, res) => {
+http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write("Hello and welcome to CMS");
+    res.end();
 
-    
-
-}).listen(port, () => console.log("listening on port " + port));
+}).listen(port, () => console.log("listening on port " + port + ". Visit locallhost:3000"));
 
